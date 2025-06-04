7572d5f0cb7c52079402b8249cf2e632d04190cd
NikolajVester <nikolaj.vester@gmail.com>
2025-06-04 04:17:28 +0200

    removed commented

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ingestion.scraper_news import scrape_theverge_ai
 from storage.db import init_db
 
 def main():
-    #init_db()               # ensure DB exists
+    init_db()               # ensure DB exists
     scrape_theverge_ai()    # scrape, summarize, and store
 
 if __name__ == "__main__":
